9dbcb7990c8fd684423692edcd8579b2a268d911
Wills Field <wilfiel@chbc.school.nz>
2023-03-22 14:36:48 +1300

    Add project files.

---
+++ ChemicalApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//imports
+
+using System;
+
+namespace ChemicalApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
